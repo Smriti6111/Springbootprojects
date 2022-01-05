ebb7cb7a04bb9f31cacc5c6c442b81ac75aa02e7
Smriti Rani <89844743+Smriti6111@users.noreply.github.com>
2022-01-05 20:37:13 +0530

    Add files via upload

---
+++ restapi/restapi/src/main/java/com/example/restapi/CartController.java
@@ -0,0 +1,40 @@
+package com.example.restapi;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+
+public class CartController {
+	
+	@Autowired
+	CartService cartService;
+	@GetMapping("/get")
+	public List<CartItem> getCartDetails() {
+		return cartService.getCartDetails();
+		
+	}
+	@PostMapping("/add")
+	public int addCartDetails(@RequestBody CartItem cartItem) {
+		return cartService.addCartDetails(cartItem);
+	}
+	@PutMapping("/update")
+	public int updateCartDetails(@RequestBody CartItem cartItem) {
+		return cartService.updateCartDetails(cartItem);
+	}
+	@DeleteMapping("/delete/{id}")
+	public int deleteCartDetails(@PathVariable int id) {
+		return cartService.deleteCartDetails(id);
+	}
+
+}
---
+++ restapi/restapi/src/main/java/com/example/restapi/CartRepository.java
@@ -0,0 +1,39 @@
+package com.example.restapi;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CartRepository {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public List<CartItem> getcartDetails() {
+		return jdbcTemplate.query("select * from carts",
+				(rs, rowNum) -> 
+		                new CartItem(
+		                		rs.getInt("id"),
+		                		rs.getString("name"),
+		                		rs.getInt("price"))
+				);
+	}
+	
+	public int addCartDetails(CartItem cartItem) {
+	return jdbcTemplate.update("insert into carts(id, name, price) values(?,?,?) ",
+				cartItem.getId(),cartItem.getName(),cartItem.getPrice());
+	}
+	
+	public int updateCartDetails(CartItem cartItem) {
+	return jdbcTemplate.update("update carts set name= ? where id= ?",
+					cartItem.getName(),cartItem.getId());
+		}
+	
+	public int deleteCartDetails(int id) {
+		return jdbcTemplate.update(" delete carts where id= ?", id);
+						
+			}
+}
