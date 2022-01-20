6a4cae76402dd516541ce0534d867b414b796e64
Smriti Rani <89844743+Smriti6111@users.noreply.github.com>
2022-01-20 16:34:09 +0530

    Add files via upload

---
+++ Springbootjpapostman/src/main/java/com/example/jpa/model/Alien.java
@@ -0,0 +1,62 @@
+package com.example.jpa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+//by giving entity and id table will be automatically created in the h2 database
+//we can directly do 
+//select * from alien;
+//insert into ALIEN VALUES(101,'Navin','java');
+//insert into ALIEN VALUES(102,'Kiran','android');
+//insert into ALIEN VALUES(103,'Pravin','python');
+//insert into ALIEN VALUES(104,'Komal','java');
+//insert into ALIEN VALUES(105,'Rafiq','android');
+//101	Navin	java
+//102	kiran	andriod
+//103	pravin	python
+//104	komal	java
+//105	rafiq	       android
+
+@Entity
+public class Alien {
+	@Id
+  private int aid;
+  private String aname;
+  private String tech;
+public int getAid() {
+	return aid;
+}
+public void setAid(int aid) {
+	this.aid = aid;
+}
+public String getAname() {
+	return aname;
+}
+public void setAname(String aname) {
+	this.aname = aname;
+}
+public String getTech() {
+	return tech;
+}
+public void setTech(String tech) {
+	this.tech = tech;
+}
+@Override
+public String toString() {
+	return "Alien [aid=" + aid + ", aname=" + aname + ", tech=" + tech + "]";
+}
+   
+}
+//localhost:8080/aliens will give all the aliens present in database in json format
+//localhost:8080/alien/103 will give specific alien details in json format
+//localhost:8080/aliens to get data in json format in postman select get
+//localhost:8080/aliens to get data in xml format in postman...
+//in headers key=Accept,application/xml,send,xml is not supported by springboot so it will give 406 not acceptable
+//to make xml supportable dependency is added then it will give xml format in output
+
+//http://localhost:8080/alien/103 in json format by get 
+// we can also post from postman ...localhost:8080/alien....body...formdata...key=aid,value=106,key=aname,value=Priya,key=tech,value=ML
+
+//localhost:8080/alien/106
+//localhost:8080/h2-console
+
+
