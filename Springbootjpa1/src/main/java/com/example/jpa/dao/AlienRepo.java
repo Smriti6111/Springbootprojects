29053e4a0007c0b3b85ba1775b4f9ce7a083bd82
Smriti Rani <89844743+Smriti6111@users.noreply.github.com>
2022-01-25 10:17:08 +0530

    Delete Springbootjpa1 directory

--- Springbootjpa1/src/main/java/com/example/jpa/dao/AlienRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpa.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.jpa.model.Alien;
-
-public interface AlienRepo extends CrudRepository<Alien,Integer>{
-
-}
